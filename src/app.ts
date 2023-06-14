287608393c0385475363f30b6e3f38ba638caa53
Diogo Vasques <divasques12.11@gmail.com>
2023-06-14 16:42:01 -0300

    feat: created basic express server structure

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from 'express'
+import cors from "cors"
+import dotenv from "dotenv"
+import routes from '@/routes'
+
+dotenv.config()
+
+const app: Express = express()
+app.use(express.json())
+app.use(cors())
+
+app.use('/api', routes)
+
+export default app
