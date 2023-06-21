f0b9ae3af7510792bd8c8b4499ab2341dd86ab05
Diogo Vasques <divasques12.11@gmail.com>
2023-06-21 03:08:23 -0300

    refactor: implemented unit tests

--- tests/mocks/User.ts
+++ tests/mocks/User.ts
@@ -4,8 +4,8 @@ export const user: User = {
     _id: '1234',
     name: 'Diogo',
     email: 'email@email.com',
-    balance: 0,
-    totalPoints: 0,
+    balance: 15000,
+    totalPoints: 15000,
     createdAt: new Date('2043-06-19T02:52:14.474Z'),
     updatedAt: new Date('2043-06-19T02:52:14.474Z')
 }
--- tests/services/RewardService.spec.ts
+++ tests/services/RewardService.spec.ts
@@ -1,21 +1,31 @@
 import { RewardService } from '@/services/RewardService'
 import { Reward } from '@/models/Reward'
 import { reward, rewardList } from '../mocks/Reward'
+import { user } from '../mocks/User'
 import AppError from '@/models/error/AppError'
 
 describe('RewardService', () => {
     let rewardService: RewardService
     let rewardRepositoryMock: any
+    let userRepositoryMock: any
 
     beforeEach(() => {
         rewardRepositoryMock = {
             find: jest.fn().mockResolvedValue(rewardList),
             create: jest.fn(),
             findById: jest.fn().mockResolvedValue(reward),
-            delete: jest.fn()
+            delete: jest.fn(),
+            claimReward: jest.fn().mockResolvedValue(1),
+            handReward: jest.fn().mockResolvedValue(1),
+            rollbackClaim: jest.fn()
+        }
+        userRepositoryMock = {
+            findById: jest.fn().mockResolvedValue(user),
+            withdrawPoints: jest.fn().mockResolvedValue(1)
         }
         rewardService = new RewardService()
         rewardService['rewardRepository'] = rewardRepositoryMock
+        rewardService['userRepository'] = userRepositoryMock
     })
 
     afterEach(() =>
@@ -107,6 +117,220 @@ describe('RewardService', () => {
             // Assert
             expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
             expect(rewardRepositoryMock.delete).toHaveBeenCalledWith(id)
+        })
+    })
+
+    describe('claimReward', () => {
+        it('should throw an error if the reward was not found', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+            rewardRepositoryMock.findById.mockResolvedValue(null)
+            let error
+
+            // Act
+            try {
+                await rewardService.claimReward(id, userId)
+            } catch (e) {
+                error = e
+            }
+            
+            // Assert
+            expect(error).toBeInstanceOf(AppError)
+            expect((error as AppError).status).toBe(404)
+            expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
+            expect(rewardRepositoryMock.claimReward).not.toHaveBeenCalled()
+            expect(userRepositoryMock.withdrawPoints).not.toHaveBeenCalled()
+            expect(rewardRepositoryMock.rollbackClaim).not.toHaveBeenCalled()
+        })
+        it('should throw an error if the user was not found', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+            userRepositoryMock.findById.mockResolvedValue(null)
+            let error
+
+            // Act
+            try {
+                await rewardService.claimReward(id, userId)
+            } catch (e) {
+                error = e
+            }
+            
+            // Assert
+            expect(error).toBeInstanceOf(AppError)
+            expect((error as AppError).status).toBe(404)
+            expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId)
+            expect(rewardRepositoryMock.claimReward).not.toHaveBeenCalled()
+            expect(userRepositoryMock.withdrawPoints).not.toHaveBeenCalled()
+            expect(rewardRepositoryMock.rollbackClaim).not.toHaveBeenCalled()
+        })
+
+        it('should throw an error if the user does not have sufficient balance', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+            userRepositoryMock.findById.mockResolvedValue({...user, balance: 0})
+            let error
+
+            // Act
+            try {
+                await rewardService.claimReward(id, userId)
+            } catch (e) {
+                error = e
+            }
+            
+            // Assert
+            expect(error).toBeInstanceOf(AppError)
+            expect((error as AppError).status).toBe(400)
+            expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
+            expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId)
+            expect(rewardRepositoryMock.claimReward).not.toHaveBeenCalled()
+            expect(userRepositoryMock.withdrawPoints).not.toHaveBeenCalled()
+            expect(rewardRepositoryMock.rollbackClaim).not.toHaveBeenCalled()
+        })
+
+        it('should throw an error if the user already claimed the reward', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+            rewardRepositoryMock.claimReward.mockResolvedValue(0)
+            let error
+
+            // Act
+            try {
+                await rewardService.claimReward(id, userId)
+            } catch (e) {
+                error = e
+            }
+            
+            // Assert
+            expect(error).toBeInstanceOf(AppError)
+            expect((error as AppError).status).toBe(400)
+            expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
+            expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId)
+            expect(rewardRepositoryMock.claimReward).toHaveBeenCalledWith(id, userId)
+            expect(userRepositoryMock.withdrawPoints).not.toHaveBeenCalled()
+            expect(rewardRepositoryMock.rollbackClaim).not.toHaveBeenCalled()
+        })
+
+        it('should throw an error and rollback claim if could not withdraw points', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+            userRepositoryMock.withdrawPoints.mockResolvedValue(0)
+            let error
+
+            // Act
+            try {
+                await rewardService.claimReward(id, userId)
+            } catch (e) {
+                error = e
+            }
+            
+            // Assert
+            expect(error).toBeInstanceOf(AppError)
+            expect((error as AppError).status).toBe(400)
+            expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
+            expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId)
+            expect(rewardRepositoryMock.claimReward).toHaveBeenCalledWith(id, userId)
+            expect(userRepositoryMock.withdrawPoints).toHaveBeenCalledWith(userId, reward.price)
+            expect(rewardRepositoryMock.rollbackClaim).toHaveBeenCalledWith(id, userId)
+        })
+
+        it('should claim the reward', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+
+            // Act
+            await rewardService.claimReward(id, userId)
+
+            // Assert
+            expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
+            expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId)
+            expect(rewardRepositoryMock.claimReward).toHaveBeenCalledWith(id, userId)
+            expect(userRepositoryMock.withdrawPoints).toHaveBeenCalledWith(userId, reward.price)
+            expect(rewardRepositoryMock.rollbackClaim).not.toHaveBeenCalled()
+        })
+    })
+
+    describe('handReward', () => {
+        it('should throw an error if the reward was not found', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+            rewardRepositoryMock.findById.mockResolvedValue(null)
+            let error
+
+            // Act
+            try {
+                await rewardService.handReward(id, userId)
+            } catch (e) {
+                error = e
+            }
+            
+            // Assert
+            expect(error).toBeInstanceOf(AppError)
+            expect((error as AppError).status).toBe(404)
+            expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
+            expect(rewardRepositoryMock.handReward).not.toHaveBeenCalled()
+        })
+        it('should throw an error if the user was not found', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+            userRepositoryMock.findById.mockResolvedValue(null)
+            let error
+
+            // Act
+            try {
+                await rewardService.handReward(id, userId)
+            } catch (e) {
+                error = e
+            }
+            
+            // Assert
+            expect(error).toBeInstanceOf(AppError)
+            expect((error as AppError).status).toBe(404)
+            expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId)
+            expect(rewardRepositoryMock.handReward).not.toHaveBeenCalled()
+        })
+
+        it('should throw an error if the user did not claim the reward', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+            rewardRepositoryMock.handReward.mockResolvedValue(0)
+            let error
+
+            // Act
+            try {
+                await rewardService.handReward(id, userId)
+            } catch (e) {
+                error = e
+            }
+            
+            // Assert
+            expect(error).toBeInstanceOf(AppError)
+            expect((error as AppError).status).toBe(400)
+            expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
+            expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId)
+            expect(rewardRepositoryMock.handReward).toHaveBeenCalledWith(id, userId)
+        })
+
+        it('should hand the reward', async () => {
+            // Arrange
+            const id = '123'
+            const userId = '456'
+
+            // Act
+            await rewardService.handReward(id, userId)
+
+            // Assert
+            expect(rewardRepositoryMock.findById).toHaveBeenCalledWith(id)
+            expect(userRepositoryMock.findById).toHaveBeenCalledWith(userId)
+            expect(rewardRepositoryMock.handReward).toHaveBeenCalledWith(id, userId)
         })
     })
 })
