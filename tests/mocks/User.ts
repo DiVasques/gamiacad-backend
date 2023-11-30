9e0d01b51b1165475d4c230f059d9bfc46772b34
Diogo Vasques <divasques12.11@gmail.com>
2023-11-30 04:02:19 -0300

    feat: added user privilege on get users endpoint

--- tests/mocks/User.ts
+++ tests/mocks/User.ts
@@ -1,4 +1,5 @@
 import { User } from '@/models/User'
+import { UserWithPrivilege } from '@/models/UserWithPrivilege'
 
 export const user: User = {
     _id: '1b6ec50e-9be1-459f-bd69-11bfa325d03b',
@@ -11,7 +12,17 @@ export const user: User = {
     updatedAt: new Date('2043-06-19T02:52:14.474Z')
 }
 
+export const userWithPrivilege: UserWithPrivilege = {
+    ...user,
+    admin: true
+}
+
 export const userList: User[] = [
     user,
     { ...user, name: 'Foo', _id: '12345' }
+]
+
+export const userWithPrivilegeList: UserWithPrivilege[] = [
+    userWithPrivilege,
+    { ...userWithPrivilege, name: 'Foo', _id: '12345' }
 ]
